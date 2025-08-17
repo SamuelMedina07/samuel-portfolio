93d6d7ad43627fa1be3355c398186f7e725d7903
SamuelMedina07 <semedina@uth.hn>
2025-08-16 21:23:02 -0600

    Add portfolio missing proyects

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
